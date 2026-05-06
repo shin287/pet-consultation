cd71a8fcad7f24c1c095747415d4dd4d696f3df2
shin287 <sho-rinjikempo@outlook.jp>
2026-05-06 19:03:49 +0900

    Initial commit

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,36 @@
+class QuestionsController < ApplicationController
+  def index
+    @questions = Question.all
+  end
+
+  def show
+    @question = Question.find(params[:id])
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(question_params)
+    @question.user_id = User.first.id
+
+    if @question.save
+      redirect_to questions_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @question = Question.find(params[:id])
+    @question.destroy
+    redirect_to questions_path
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :body, :category)
+  end
+end
