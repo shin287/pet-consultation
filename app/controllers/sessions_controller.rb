99aaa8c44cdfbcf89b66f7cac535211dcdb3a7e3
shin287 <sho-rinjikempo@outlook.jp>
2026-05-09 16:54:41 +0900

    ゲストログイン機能追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,6 +15,12 @@ class SessionsController < ApplicationController
     end
   end
 
+  def guest
+    user = User.guest
+    session[:user_id] = user.id
+    redirect_to questions_path
+  end
+
   def destroy
     session[:user_id] = nil
     redirect_to login_path
