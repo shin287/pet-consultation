397f06cd40d866d6f63964e4fa0a7dc46ceaf482
shin287 <sho-rinjikempo@outlook.jp>
2026-05-14 01:44:29 +0900

    再々修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,6 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   config.active_job.queue_adapter = :inline
+  config.active_storage.queues.analysis = nil
   config.active_storage.queues.purge = nil
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -52,7 +53,7 @@ Rails.application.configure do
   config.cache_store = :solid_cache_store
 
   # Replace the default in-process and non-durable queuing backend for Active Job.
-  config.active_job.queue_adapter = :solid_queue
+  # config.active_job.queue_adapter = :solid_queue
   config.solid_queue.connects_to = { database: { writing: :queue } }
 
   # Ignore bad email addresses and do not raise email delivery errors.
