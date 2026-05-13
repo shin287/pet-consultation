e64e72c697352f2ecb5ef2515a0a98769032c7f6
shin287 <sho-rinjikempo@outlook.jp>
2026-05-14 01:17:35 +0900

    本番環境のジョブ設定を修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  config.active_job.queue_adapter = :inline
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
